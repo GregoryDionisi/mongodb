b4b9569fbe146e5043bf078e8c14f34828c1cae2
DIONISI GREGORY AARON <77567516@castelli.local>
2025-03-18 11:24:18 +0100

    Inizio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const cors = require(`cors`);
 const bodyParser = require(`body-parser`);
 const {MongoClient} = require(`mongodb`);
- 
+require('dotenv').config();
  
 const app = express();
 app.use(cors());
