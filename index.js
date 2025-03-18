ca168990feb71222e7ebf85d9905076db3acb37b
DIONISI GREGORY AARON <77567516@castelli.local>
2025-03-18 11:37:55 +0100

    Inizio

--- index.js
+++ index.js
@@ -9,5 +9,23 @@ app.use(cors());
 app.use(bodyParser.json())
 
 const connectToDatabase = async () => {
-    const client = await MongoClient.connect()
+    try{
+        const client = await MongoClient.connect(process.env.MONGO_URI) //process del file appena creato e la variabile a cui hai associato il link
+        console.log('Connected to database');
+        return client.db('mongodb'); //mongodb nome del nostro database
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
 }
+
+let database
+
+const startServer = async () => {
+    database = await connectToDatabase();
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+}
+
+startServer();
