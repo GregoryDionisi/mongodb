5445973effbcb39771a4117aff6ad0058885b5d6
DIONISI GREGORY AARON <77567516@castelli.local>
2025-03-18 11:19:20 +0100

    Inizio

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require(`express`);
+const cors = require(`cors`);
+const bodyParser = require(`body-parser`);
+const {MongoClient} = require(`mongodb`);
+ 
+ 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+
+const connectToDatabase = async () => {
+    const client = await MongoClient.connect()
+}
