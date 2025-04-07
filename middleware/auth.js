63ec2d0a3430ba4382b0b4cb4ac1c394044f6531
DIONISI GREGORY AARON <77567516@castelli.local>
2025-04-07 11:50:23 +0200

    Aggiunta middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,31 @@
-//localhost:3000/profile?user=luca
-
-const auth = (req, res, next) => {
-    const {user} = req.query
-    console.log(user)
-    if (user === "luca"){
-    next()
-    }
-    else{
-        res.send("ACCESS DENIED")
-    }
-}
-
-module.exports = auth
+    //localhost:300/profile?user=luca può accedere solo se si chima luca
+ 
+    const auth = (req, res, next) => {
+        const {user} = req.query
+        console.log(user);
+        const oraCorrente = new Date().getHours(); // ottieni l'ora attuale
+     
+        if (oraCorrente >= 10 && oraCorrente < 11) {
+             res.send("ACCESSO NEGATO: Il servizio non è disponibile tra le 10 e le 12");
+        } else {
+            res.send("accesso consentito")
+        }
+     
+     
+     
+        // if (user === "luca"){
+        //     next()
+        // }
+        // else {
+        //     res.send("ACCESSO NEGATO ")
+        // }
+       
+     
+            // next()  // dopo essere enrtsto nella rotta fai questo
+        }
+     
+     
+     
+        module.exports = auth
+     
+        //crea middleware che blocchi accessi che vengo effettuati accessi tra 10 e 12
